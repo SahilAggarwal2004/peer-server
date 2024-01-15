414f0e860ab0c8dc52a25650f1fd645d60c19e2f
SahilAggarwal2004 <aggarwalsahil2004@gmail.com>
2024-01-15 17:09:04 +0530

    commit

--- index.js
+++ index.js
@@ -1,6 +1,2 @@
 const { PeerServer } = require('peer');
-
-const port = process.env.PORT || 5100
-if (port === 5100) require('dotenv').config({ path: './.env' })
-
-PeerServer({ port, corsOptions: { origin: [process.env.CORS], maxAge: 600 } })
+PeerServer({ port: process.env.PORT || 5100, corsOptions: { origin: [process.env.CORS], maxAge: 600 } })
